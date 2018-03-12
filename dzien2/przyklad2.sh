f6e39f9074822aa7af1685dcc6d83e4ccb245fae
gkoziel <gkoziel@grupainteger.pl>
2018-03-12 20:47:04 +0100

    rename

--- dzien2/przyklad2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-DANE=”$(ls -l)”
-echo ”Wynik metody rozwijania zawartosci nawiasow:”
-echo ”$DANE”
-DANE=`cat test.txt`
-echo ”Wynik metody ujmowania polecenia w tzw. wsteczne apostrofy:”
-echo ”$DANE”
--- dzien2/przyklad9.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-tablica=(element1 element2 element3)
-tablica[3]=element4
-
-echo ${tablica[@]}
